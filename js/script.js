fde3313d03912ea53ad6b9cec18bd5f285fb323c
斉藤亜子 <saitouako@Akos-MacBook-Pro.local>
2019-09-30 00:06:25 +0900

    add works

--- js/script.js
+++ js/script.js
@@ -12,18 +12,6 @@ $(function () {
   var navHeight = $nav.innerHeight();
   var headerInner = $('#header .inner');
 
-  //ページ内スクロール
-  $('a[href^="#"]').on('click', function () {
-    var speed = 300;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top - navHeight;
-    $("html, body").animate({
-      scrollTop: position
-    }, speed, "swing");
-    return false;
-  });
-
   //ページトップへもどる
   $('#js-pageTop').on('click', function () {
     $('body,html').animate({
